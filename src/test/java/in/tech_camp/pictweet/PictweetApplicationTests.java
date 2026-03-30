38aa4c9f0475d836972036aa039550c6bdea5fc7
Asuka Toyoshima <asuka.toyoshima@di-v.co.jp>
2026-03-30 19:03:42 +0900

    PictweetApplicationTestsにActiveProfiles追加

--- src/test/java/in/tech_camp/pictweet/PictweetApplicationTests.java
+++ src/test/java/in/tech_camp/pictweet/PictweetApplicationTests.java
@@ -2,8 +2,10 @@ package in.tech_camp.pictweet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PictweetApplicationTests {
 
 	@Test
